985855bb21bd30cda25b10889e30ea88e7ff9e3c
Abdullah Alsaati <abdullah.alsaati@hotmail.com>
2016-02-23 13:13:52 +0000

    initial commit get stuck in loading screen when pressing control

---
+++ app/build/generated/source/buildConfig/flavor1/debug/com/parrot/freeflight/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.parrot.freeflight;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.app";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "flavor1";
+  public static final int VERSION_CODE = 20000;
+  public static final String VERSION_NAME = "";
+  // Fields from default config.
+  public static final int VALUE = 1;
+}
